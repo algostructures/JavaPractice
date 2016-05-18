2d1075ae50bb9b096fdfeb2b5eef4b3d263a9911
saurabh prajapati <saurabh945644@gmail.com>
2016-05-18 07:34:24 +0530

    Create Test.java

---
+++ DataStructure/ExpressionEvalutaion/Test.java
@@ -0,0 +1,10 @@
+/*Driver Method for expression evaluation*/
+public class Test {
+
+	public static void main(String[] args) {
+		EvaluateExpression e=new EvaluateExpression("3+2");
+		e.toPostfix();
+	}
+}
+
+
